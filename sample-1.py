cb0728ab4e7fc15b8f54d1a1ae6c67d09965b9f2
Arijit Ain Das <aaindas@hotmail.com>
2024-04-18 00:31:22 -0400

    wip

--- sample-1.py
+++ sample-1.py
@@ -22,23 +22,51 @@ class Auto:
         self.make = make
         self.model = model
         
-    def print(self):
+    def get_details(self):
         print(f"Auto: {self.make} {self.color} {self.model} wheels: {self.wheels}")
         
+    @staticmethod
+    def get_top_dealers(n):
+
+        auto_dealers = [
+            {"name": "Dealer 1", "rating": 4.5},
+            {"name": "Dealer 2", "rating": 3.8},
+            {"name": "Dealer 3", "rating": 4.2},
+            {"name": "Dealer 4", "rating": 4.9},
+            {"name": "Dealer 5", "rating": 3.7},
+            # Add more dealers here...
+        ]    
+        sorted_dealers = sorted(auto_dealers, key=lambda x: x["rating"], reverse=True)
+        
+        return sorted_dealers[:n]
+
+            
 toyota = Auto(Color.WHITE, Manufacturer.TOYOTA, "Corolla")
 honda = Auto(Color.BLACK, Manufacturer.HONDA, "Civic")
 
-toyota.print()
-honda.print()
+toyota.get_details()
+honda.get_details()
 
 # Set wheels to 6
 Auto.wheels = 6
 
-toyota.print()
-honda.print()
+toyota.get_details()
+honda.get_details()
 
 # Set wheels to 8
 toyota.wheels = 8
 
-toyota.print()
-honda.print()
+toyota.get_details()
+honda.get_details()
+
+# Class usage: Get the top 3 dealers
+top_dealers = Auto.get_top_dealers(3)
+for dealer in top_dealers:
+    print(dealer["name"], "- Rating:", dealer["rating"])
+
+print("-----")
+   
+# Instance usage: Get the top 3 dealers
+top_dealers = toyota.get_top_dealers(3)
+for dealer in top_dealers:
+    print(dealer["name"], "- Rating:", dealer["rating"])    
